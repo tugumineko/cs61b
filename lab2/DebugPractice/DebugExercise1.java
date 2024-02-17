98964c40136c5296ee9e11fc5f009252f6c1edce
tugumineko <exd422@gmail.com>
2024-02-17 22:15:52 +0800

    lab2 completed IntListTest

---
+++ lab2/DebugPractice/DebugExercise1.java
@@ -0,0 +1,28 @@
+/**
+ * Exercise for learning how the debug, breakpoint, and step-into
+ * feature work.
+ */
+public class DebugExercise1 {
+    public static long divideThenRound(double top, double bottom) {
+        double quotient = top / bottom;
+        long result = Math.round(quotient);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int t = 10;
+        int b = 2;
+        long result = divideThenRound(t, b);
+        System.out.println("round(" + t + "/" + b + ")=" + result);
+
+        int t2 = 9;
+        int b2 = 4;
+        long result2 = divideThenRound(t2, b2);
+        System.out.println("round(" + t2 + "/" + b2 + ")=" + result2);
+
+        int t3 = 3;
+        int b3 = 4;
+        long result3 = divideThenRound(t3, b3);
+        System.out.println("round(" + t3 + "/" + b3 + ")=" + result3);
+    }
+}
