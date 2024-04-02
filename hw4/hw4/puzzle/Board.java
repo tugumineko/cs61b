7213798e004e3c838827f3b1b2328585eab5e8e9
tugumineko <exd422@gmail.com>
2024-04-02 22:15:11 +0800

    hw4 Board completed

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -127,6 +127,11 @@ public class Board implements WorldState{
         return true;
     }
 
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
     /** Returns the string representation of the board.
       * Uncomment this method. */
     public String toString() {
